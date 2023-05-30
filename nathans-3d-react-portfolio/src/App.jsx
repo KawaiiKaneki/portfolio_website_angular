e252fb646cfe0e09d29908078991ee4339faa8c1
Nathan Warren <nathan.warren@thedatacompany.com>
2023-05-30 14:12:55 +0200

    completed projects page

--- nathans-3d-react-portfolio/src/App.jsx
+++ nathans-3d-react-portfolio/src/App.jsx
@@ -1,29 +1,43 @@
-
+import React, { useState, useEffect, Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
-import { Suspense } from 'react'
 import {Loader} from '@react-three/drei'
-import Projects from './components/Projects/project'
+import Project from './components/Projects/project'
+import ProjectMobile from './components/Projects/ProjectMobile'
 
 function App() {
-  
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    <>
       <BrowserRouter>
-        <Navbar/>
+      <Navbar />
+      <Suspense fallback={null}>
+        <Hero />
+      </Suspense>
+      <Loader/>
+      <About />
+      {isDesktop ? (
         <Suspense fallback={null}>
-          <Hero/>
+          <Project />
         </Suspense>
-        <About/>
-        <Loader/>
-        <Suspense fallback={null}>
-          <Projects />
-        </Suspense>
+      ):(
+          <ProjectMobile/>
+      )}
       </BrowserRouter>
-    </>
   )
 }
 
